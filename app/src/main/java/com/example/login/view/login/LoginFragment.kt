c032696318a02af6e1ce76c638499aa67f2a6280
pedro-donoso <pedro.donoso.r@usach.cl>
2024-07-18 11:43:37 -0400

    agrego rutas home y register

--- app/src/main/java/com/example/login/view/login/LoginFragment.kt
+++ app/src/main/java/com/example/login/view/login/LoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.login.R
 import com.example.login.databinding.FragmentLoginBinding
+import com.example.login.databinding.FragmentRegisterBinding
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -17,6 +20,25 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navegar al HomeFragment al hacer clic en el botón de login
+        binding.loginButton.setOnClickListener {
+        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
